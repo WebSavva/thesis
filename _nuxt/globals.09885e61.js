ccdea4430fd5044775448a8df2bba17551634536
WebSavva <WebSavva@users.noreply.github.com>
2023-07-30 22:28:29 +0000

    deploy: 00b20b31ec0f711db34ed2f0105a73e3644ca753

---
+++ _nuxt/globals.09885e61.js
@@ -0,0 +1,1 @@
+import{O as e}from"./entry.3f7ae044.js";const r=t=>()=>{const n=e();if(!n)throw new Error("[ int ]: No instance is found !");return n.proxy[t]},s=r("$baseUrl"),a=r("$int");export{s as a,a as u};
--- _nuxt/use-internalization.1d170f5d.js
+++
@@ -1,1 +0,0 @@
-import{O as r}from"./entry.4478c12f.js";const e=()=>{const n=r();if(!n)throw new Error("[ int ]: No instance is found !");const{$int:t}=n.proxy;return t};export{e as u};
